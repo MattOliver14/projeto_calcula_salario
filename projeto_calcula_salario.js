c246ee6b6aa6b0b88e580761e59e26c15dff4b58
Matheus Oliveira <matt.oliver.game@gmail.com>
2021-06-21 18:15:00 -0300

    Primeiro Commit

---
+++ projeto_calcula_salario.js
@@ -0,0 +1,14 @@
+function calculaSalario(){
+        var valor = document.getElementById("valor").value;
+        var a = 100
+        var b = 10
+        var salario = valor/a*b
+        
+       alert(salario);
+}       
+
+
+
+
+
+<script type="text/javascript" src="projeto_calcula_salario" > </script>
